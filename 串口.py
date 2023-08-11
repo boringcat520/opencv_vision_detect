a89d90dbcb2e262686254499041ca35bcdbc0eed
boringcat520 <194739269@qq.com>
2023-08-11 10:58:22 +0800

    串口修改

--- 串口.py
+++ 串口.py
@@ -1,11 +1,15 @@
 import serial
-
-
+import time
+import struct
 
 def send_data(data):#串口发送数据
     if data is not None:
         #data=numbers_process(data)
         ser = serial.Serial("/dev/ttyAMA0", 115200)#set up serial
-        ser.write(data.encode('utf-8'))#write data
+        ser.write(data)#write data
 
-send_data('1')
+while True:
+    time.sleep(0.1)
+    send_data(b'Hello world!')
+    data_packet = struct.pack('<2s6f2s', b'\x0D\x0A',0.0, 0.0, 0.0, 3.14, 42, 0.0, b'\x0A\x0D')
+    #send_data(data_packet)
