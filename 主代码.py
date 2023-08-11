47a0f196cfc6a8063346697018bcbb617cd22d43
Clark Paet <194739269@qq.com>
2023-08-11 10:55:28 +0800

    主代码更新

--- 主代码.py
+++ 主代码.py
@@ -192,6 +192,163 @@ def process_video_QRcode():  # 处理二维码
     capture.release()
     cv.destroyAllWindows()
 
+def color_det():  # 颜色检测
+
+    blue_lower = np.array([83, 27, 0])
+    blue_upper = np.array([111, 255, 255])
+    red_lower = np.array([0, 47, 177])
+    red_upper = np.array([180, 255, 255])
+    green_lower = np.array([54, 47, 35])
+    green_upper = np.array([89, 255, 255])
+
+    # 打开摄像头
+    cap = cv.VideoCapture(0)
+
+    # 检查摄像头是否成功打开
+    if not cap.isOpened():
+        print("无法打开摄像头")
+        exit()
+
+    while True:
+        # 读取摄像头画面
+        ret, frame = cap.read()
+
+        # 获取画面中心100*100区域
+        height, width, _ = frame.shape
+        center_x, center_y = int(width/2), int(height/2)
+        roi = frame[center_y-50:center_y+50, center_x-50:center_x+50]
+
+        # 将区域转换为HSV格式
+        hsv = cv.cvtColor(roi, cv.COLOR_BGR2HSV)
+
+        # 在区域中提取蓝色、红色、绿色区域
+        blue_mask = cv.inRange(hsv, blue_lower, blue_upper)
+        red_mask = cv.inRange(hsv, red_lower, red_upper)
+        green_mask = cv.inRange(hsv, green_lower, green_upper)
+
+        # 计算蓝色、红色、绿色区域的像素数量
+        blue_pixels = cv.countNonZero(blue_mask)
+        red_pixels = cv.countNonZero(red_mask)
+        green_pixels = cv.countNonZero(green_mask)
+        color = "unkonw"
+        # 判断哪种颜色占据了最多的像素数量
+        if blue_pixels > red_pixels and blue_pixels > green_pixels:
+            color = "blue"
+        elif red_pixels > blue_pixels and red_pixels > green_pixels:
+            color = "red"
+        elif green_pixels > blue_pixels and green_pixels > red_pixels:
+            color = "green"
+        else:
+            color = "unkonw"
+        print(blue_pixels, red_pixels, green_pixels)
+        # 在画面中心显示检测结果
+        cv.putText(frame, color, (center_x-50, center_y-60), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        cv.rectangle(frame, (center_x-50, center_y-50), (center_x+50, center_y+50), (0, 255, 0), 2)
+        cv.imshow("frame", frame)
+
+        # 检测键盘输入，按下"q"键或"Esc"键退出程序
+        key = cv.waitKey(1)
+        if key == ord('q') or key == 27:
+            break
+
+        # 释放摄像头资源
+        cap.release()
+
+# 关闭所有OpenCV创建的窗口
+cv.destroyAllWindows()
+
+def line_det():  # 直线检测
+    cap = cv.VideoCapture(0)  # 打开摄像头
+    new_width =1280
+    new_height =960
+    cap.set(cv.CAP_PROP_FRAME_WIDTH, new_width)  # 设置摄像头帧宽度
+    cap.set(cv.CAP_PROP_FRAME_HEIGHT, new_height)  # 设置摄像头帧高度
+
+
+
+    while True:
+        left_center_x_list = []  # 存储x坐标
+        right_center_x_list = []  # 存储x坐标
+
+        left_center_y_list = []
+        right_center_y_list = []  # 存储y坐标
+        ret, frame = cap.read()  # 读取视频帧
+        #cv2.imshow("img",frame)
+        kernel = np.ones((13, 13), np.uint8)
+        img_dilation = cv.dilate(frame, kernel, iterations=1)  # 膨胀操作
+        img_erosion = cv.erode(frame, kernel, iterations=1)  # 腐蚀操作
+        gray = cv.cvtColor(img_erosion, cv.COLOR_BGR2GRAY)  # 将图像转换为灰度图
+        #cv2.imshow("gray", gray)
+        blur = cv.GaussianBlur(gray, (17, 17), 0)  # 高斯模糊
+        ret, thresh = cv.threshold(blur, 100, 255, cv.THRESH_BINARY)  # 二值化图像
+        #cv2.imshow("thresh", thresh)
+        edgeImage = cv.Canny(thresh, 50, 220)  # 边缘检测
+        cv.imshow("canny",edgeImage)
+
+        image_width = edgeImage.shape[1]
+
+        # 将图像分成左右两半
+        offset_x = image_width // 2
+        left_half = edgeImage[:, :offset_x]
+        right_half = edgeImage[:, offset_x:]
+
+        # 显示左右两半图像
+        #cv2.imshow("Left Half", left_half)
+        #cv2.imshow("Right Half", right_half)
+        contours_left, hierarchy = cv.findContours(image=left_half, mode=cv.RETR_TREE, method=cv.CHAIN_APPROX_NONE)  # 寻找轮廓
+        contours_right, hierarchy = cv.findContours(image=right_half, mode=cv.RETR_TREE, method=cv.CHAIN_APPROX_NONE)  # 寻找轮廓
+        frame_copy = frame.copy()  # 复制一份原始帧用于绘制结果
+
+        for cnt in contours_right:#右边
+            if 6000 < cv.contourArea(cnt) < 40000:  # 选择面积满足条件的轮廓
+
+                (right_center_x, right_center_y), (a, b), angle = cv.fitEllipse(cnt)
+                #cv2.ellipse(frame_copy, ellipse, (0, 0, 255), 2)  # 在复制的帧上绘制椭圆
+
+                ellipse=(right_center_x + offset_x, right_center_y),(a, b), angle
+                right_center_x_list.append(right_center_x + offset_x)  # 将中心点的x坐标添加到列表中
+                right_center_y_list.append(right_center_y)  # 将中心点的y坐标添加到列表中
+                cv.ellipse(frame_copy, ellipse, (0, 0, 255), 2)  # 在复制的帧上绘制椭圆
+                #print(f'Circle Center: ({center_x}, {center_y})')
+
+        for cnt in contours_left:#左边
+            if 7000 < cv.contourArea(cnt) < 30000:  # 选择面积满足条件的轮廓
+                ellipse = cv.fitEllipse(cnt)  # 拟合椭圆
+                cv.ellipse(frame_copy, ellipse, (0, 0, 255), 2)  # 在复制的帧上绘制椭圆
+                (left_center_x, left_center_y), _, _ = ellipse
+                left_center_x_list.append(left_center_x)  # 将中心点的x坐标添加到列表中
+                left_center_y_list.append(left_center_y)  # 将中心点的y坐标添加到列表中
+
+                #print(f'Circle Center: ({center_x}, {center_y})')
+
+        cv.waitKey(1)
+
+        cv.imshow("frame", frame_copy)  # 显示帧
+
+
+
+        # 计算直线的左右端点像素位置
+        if len(left_center_x_list)>=2:
+          
+            left_point_x = np.mean(left_center_x_list)
+            right_point_x = np.mean(right_center_x_list)
+
+            left_point_y = np.mean(left_center_y_list)
+            right_point_y = np.mean(right_center_y_list)
+                # 计算直线的角度
+            slope = (right_point_y - left_point_y) / (right_point_x - left_point_x)
+            angle = np.arctan(slope) * 180 / np.pi
+
+            print(f'Left Point: ({left_point_x}, {left_point_y})')
+            print(f'Right Point: ({right_point_x}, {right_point_y})')
+            print(f'Angle: {angle}')
+            if cv.waitKey(1) & 0xFF == ord('q'):  # 如果按下 'q' 键则退出循环
+                break
+
+
+    cap.release()  # 释放摄像头
+    cv.destroyAllWindows()  # 关闭窗口
+    
 
 def send_data(data):  # 串口发送数据，data为字符串
     if data is not None:
